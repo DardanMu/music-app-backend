63aefde476bd31f7e5eb5068e347c5a20c184985
Dardan <Dardan.mu@gmail.com>
2014-06-21 20:07:31 +0100

    removed old comment

--- app/controllers/songs.js
+++ app/controllers/songs.js
@@ -6,10 +6,6 @@ exports.index = function(req, res){
 
 	var songs = groovesharkService.getSongs(artist, function(error, response, songs){
 		if (!error) {
-            // res.format({
-            //   "text/plain": function(){
-            //     res.send(songs);
-            // }});
             res.set('Content-Type', 'text/plain');
 			res.send(songs);
 		}else{
