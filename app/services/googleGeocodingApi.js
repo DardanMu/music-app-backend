a39737249608dab8e2df4603f468c2506bb2ca33
Dardan <Dardan.mu@gmail.com>
2014-07-20 23:02:43 +0100

    created a google geocoding api service file

---
+++ app/services/googleGeocodingApi.js
@@ -0,0 +1,23 @@
+var Q = require('q'),
+    request = Q.denodeify(require('request')),
+    params = require('../../config/parameters');
+
+var googleGeocodingApi = {
+
+	getData: function(artist, dataType) {
+		
+		if (dataType === 'info') {
+			var url = params.lastFmApi.infoUrl;
+		}else if(dataType === 'events'){
+			var url = params.lastFmApi.eventsUrl;
+		}
+
+		var apikey    = params.lastFmApi.key;
+		var urlParams = params.lastFmApi.urlParams;
+		var fullUrl   = url + artist + urlParams + apikey;
+
+		return request({uri: fullUrl, method: 'GET'});
+	}
+}
+
+module.exports = googleGeocodingApi;
